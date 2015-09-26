f1a9d907e857241905d05334f82be579ea5908a0
Kohei Yamamoto <kymmt90@gmail.com>
2015-09-26 16:27:11 +0900

    Make the sub test class in blog_entry_test.rb

--- test/blog_entry_test.rb
+++ test/blog_entry_test.rb
@@ -8,7 +8,21 @@ module Hatena
   class BlogEntryTest < Test::Unit::TestCase
     class << self
       def startup
-        @@xml = <<XML
+      end
+
+      def shutdown
+      end
+    end
+
+    def setup
+    end
+
+    def teardown
+    end
+
+    sub_test_case 'load the document from the XML text' do
+      setup do
+        @xml = <<XML
 <?xml version='1.0' encoding='UTF-8'?>
 <entry xmlns:app='http://www.w3.org/2007/app' xmlns='http://www.w3.org/2005/Atom'>
 <id>tag:blog.hatena.ne.jp,2013:blog-test_user-6653458415121899222-6653458415122161047</id>
@@ -23,71 +37,58 @@ module Hatena
 <content type='text/x-markdown'>This is the test entry.</content>
 <hatena:formatted-content xmlns:hatena='http://www.hatena.ne.jp/info/xmlns#' type='text/html'>&lt;p&gt;This is the test entry.&lt;/p&gt;
 </hatena:formatted-content>
-
 <category term='Ruby'/>
-
 <category term='Test'/>
-
 <app:control>
   <app:draft>no</app:draft>
 </app:control>
-
 </entry>
 XML
-        @@sut = BlogEntry.new(@@xml);
+        @sut = BlogEntry.new(@xml);
       end
 
-      def shutdown
+      test 'get the entry ID' do
+        assert_equal '6653458415122161047', @sut.id
       end
-    end
 
-    def setup
-    end
+      test 'get the author name' do
+        assert_equal 'test_user', @sut.author_name
+      end
 
-    def teardown
-    end
+      test 'get the title' do
+        assert_equal 'Test title', @sut.title
+      end
 
-    test 'get the entry ID' do
-      assert_equal '6653458415122161047', @@sut.id
-    end
+      test 'get the URI' do
+        assert_equal 'http://test-user.hatenablog.com/entry/2015/01/01/123456', @sut.uri
+      end
 
-    test 'get the author name' do
-      assert_equal 'test_user', @@sut.author_name
-    end
+      test 'get the edit URI' do
+        assert_equal 'https://blog.hatena.ne.jp/test_user/test-user.hatenablog.com/atom/entry/6653458415122161047', @sut.edit_uri
+      end
 
-    test 'get the title' do
-      assert_equal 'Test title', @@sut.title
-    end
+      test 'this entry is not draft' do
+        assert_false @sut.draft?
+      end
 
-    test 'get the URI' do
-      assert_equal 'http://test-user.hatenablog.com/entry/2015/01/01/123456', @@sut.uri
-    end
+      test 'get categores' do
+        assert_equal ['Ruby', 'Test'], @sut.categories
+      end
 
-    test 'get the edit URI' do
-      assert_equal 'https://blog.hatena.ne.jp/test_user/test-user.hatenablog.com/atom/entry/6653458415122161047', @@sut.edit_uri
-    end
+      test 'changing categories array does not influence to the original categories array' do
+        cats = @sut.categories
+        cats << 'Rails'
+        cats[1] = 'Sinatra'
+        assert_not_equal cats, @sut.categories
+      end
 
-    test 'this entry is not draft' do
-      assert_false @@sut.draft?
-    end
+      test 'get the content' do
+        assert_equal 'This is the test entry.', @sut.content
+      end
 
-    test 'get categores' do
-      assert_equal ['Ruby', 'Test'], @@sut.categories
-    end
-
-    test 'changing categories array does not influence to the original categories array' do
-      cats = @@sut.categories
-      cats << 'Rails'
-      cats[1] = 'Sinatra'
-      assert_not_equal cats, @@sut.categories
-    end
-
-    test 'get the content' do
-      assert_equal 'This is the test entry.', @@sut.content
-    end
-
-    test 'get the XML representation' do
-      assert_equal @@xml, @@sut.to_xml
+      test 'get the XML representation' do
+        assert_equal @xml, @sut.to_xml
+      end
     end
   end
 end
