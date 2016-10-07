4a9ad1e665c4457c581d4aefb9fad44a597889d6
Shinichi Ishimura <shiketaudonko41@gmail.com>
2016-10-08 08:04:17 +0900

    Add yard doc

--- lib/hatenablog/configuration.rb
+++ lib/hatenablog/configuration.rb
@@ -7,6 +7,9 @@ module Hatenablog
     OAUTH_KEYS = %w(consumer_key consumer_secret access_token access_token_secret user_id blog_id)
     BASIC_KEYS = %w(api_key user_id blog_id)
 
+    # Create a new configuration.
+    # @param [String] config_file configuration file path
+    # @return [Hatenablog::Configuration]
     def self.create(config_file)
       config = YAML.load(ERB.new(File.read(config_file)).result)
       keys = config['auth_type'] == 'basic' ? BASIC_KEYS : OAUTH_KEYS
